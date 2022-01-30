f5490de810dcf317acc25eb742d87592aa63c158
Jang-Jaewon <jewon119@gmail.com>
2022-01-30 19:59:20 +0900

    Add: Implement Django Basic Authentication

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from my_settings import DATABASES, SECRET_KEY
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -101,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
